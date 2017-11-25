d24ed93fa7c7e19f06e8ebcd0b05856b5334c337
Lam Ngo <lam.ngo.qu@gmail.com>
2017-11-25 19:37:47 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Suunto app
+      </div>
+    );
+  }
+}
+
+export default App;
